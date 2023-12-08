5aa8d4b5e600dbe36b56988a4503cc8b25fe17af
Happykhamissa <elmansourikhamissa@gmail.com>
2023-12-08 23:06:11 +0100

    models

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@
 """
 import uuid
 from datetime import datetime
-
+from models.__init__ import storage
 
 class BaseModel():
     """
@@ -36,6 +36,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -49,6 +50,7 @@ class BaseModel():
             - Updates updated_at with the current datetime.
         """
         self.updated_at = datetime.now()
+        storage.new(self)
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import json
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (__file_path)"""
+        serialized_objs = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, 'w') as file:
+            json.dump(serialized_objs, file)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects"""
+        try:
+            with open(self.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split('.')
+                    # Here, you'll need to create instances from the data and store them in __objects
+                    # Create instances and update __objects
+        except FileNotFoundError:
+            pass
