79855fc400fb92ed88cb9bb5a5d7d615966fe817
Happykhamissa <elmansourikhamissa@gmail.com>
2023-12-09 11:48:29 +0100

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -30,7 +30,12 @@ class BaseModel():
                     continue  # Skip '__class__'
                 if key in ('created_at', 'updated_at'):
                     # Convert string representation to datetime object
-                    setattr(self, key, datetime.strptime (value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    setattr(
+                        self,
+                        key,
+                        datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    )
+
                 else:
                     setattr(self, key, value)
         else:
